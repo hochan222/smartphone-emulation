8950b83b835d75c85464528ee260e029b6ad0c57
hochan222 <hochan049@gmail.com>
2021-08-20 19:55:07 +0900

    feat: 아래 3개의 앱이 표시된다.

---
+++ src/view/app-datas.ts
@@ -0,0 +1,16 @@
+const appDatas = [
+  {
+    name: '알람',
+    order: 1,
+  },
+  {
+    name: '메모',
+    order: 2,
+  },
+  {
+    name: '사진',
+    order: 3,
+  },
+];
+
+export default appDatas;
--- src/view/home-page.ts
+++ src/view/home-page.ts
@@ -1,15 +1,33 @@
 import { $ } from '../@shared/utils';
+import './home-page.css';
+import appInitDatas from './app-datas';
 
-const homeWrapper = (navigation: string): string => {
+const appIconWrapper = (appName: string): string => {
+  return `<button class="app-icon">${appName}</button>`;
+};
+
+const homeWrapper = (navigation: string, apps: string): string => {
   return `<div class="home">
             ${navigation}
             <div class="home-inner">
+            ${apps}
             </div>
           </div>`;
 };
 
 const renderHomePage = (navigation: string): void => {
-  $('.app')?.insertAdjacentHTML('beforeend', homeWrapper(navigation));
+  $('.app')?.insertAdjacentHTML(
+    'beforeend',
+    homeWrapper(
+      navigation,
+      appInitDatas
+        .sort((a, b) => a.order - b.order)
+        .map((appData) => {
+          return appIconWrapper(appData.name);
+        })
+        .join(''),
+    ),
+  );
 };
 
 export default renderHomePage;
