66c67606591239eb8f2f23844a893286e9d20def
hochan222 <hochan049@gmail.com>
2021-08-22 02:12:09 +0900

    feat: 상단에 날짜와 시간을 표시한다., Navigation

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,7 +1,16 @@
 import { historyRouter } from '../@shared/router';
+import { $, getCurrentDate } from '../@shared/utils';
 import homePageController from './home-page';
 
+const updateNavigationTime = () => {
+  setInterval(() => {
+    const navTimeElement = $('.nav__time') as HTMLSpanElement;
+    navTimeElement.innerText = getCurrentDate();
+  }, 1000);
+};
+
 const initController = (): void => {
+  updateNavigationTime();
   window.addEventListener('popstate', () => historyRouter(window.location.pathname));
   homePageController();
 };
