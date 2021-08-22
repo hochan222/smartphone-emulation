d2e63a277fd63d6d37eec8f70f18a4326e7bd3cb
hochan222 <hochan049@gmail.com>
2021-08-22 19:19:51 +0900

    feat: 저장된 사진을 화면에 렌더링 한다., Photo List, 사진 화면

---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+import endGame2 from './end-game-2.png';
+import endGame from './end-game.png';
+import ironMan from './iron-man.png';
+import ironMan2 from './iron-man2.png';
+import spiderMan from './spider-man.png';
+import spiderMan2 from './spider-man2.png';
+import spiderMan3 from './spider-man3.png';
+import thorRagnarok from './thor-ragnarok.png';
+import thor from './thor.png';
+import webpack from './webpack.png';
+
+const images = [
+  { name: 'end-game-2', src: endGame2 },
+  { name: 'end-game', src: endGame },
+  { name: 'iron-man', src: ironMan },
+  { name: 'iron-man2', src: ironMan2 },
+  { name: 'spider-man', src: spiderMan },
+  { name: 'spider-man2', src: spiderMan2 },
+  { name: 'spider-man3', src: spiderMan3 },
+  { name: 'thor-ragnarok', src: thorRagnarok },
+  { name: 'thor', src: thor },
+  { name: 'webpack', src: webpack },
+];
+
+export { endGame2, endGame, ironMan, ironMan2, spiderMan, spiderMan2, spiderMan3, thorRagnarok, thor, webpack, images };
--- src/view/photo-page.ts
+++ src/view/photo-page.ts
@@ -1,11 +1,22 @@
 import { $ } from '../@shared/utils';
-import webpack from '../assets/webpack.png';
+import { images } from '../assets/index';
+import './photo-page.css';
 
-const photoWrapper = (navigation: string): string => {
+const imageTagWrapper = ({ src, name }: { src: string; name: string }): string => {
+  return `<image class="photo-scroll-image" src="${src}" data-name="${name}"/>`;
+};
+
+const photoScrollWrapper = (): string => {
+  return `<div class="photo-scroll">
+            ${images.map(({ name, src }) => imageTagWrapper({ name, src }))}
+          </div>`;
+};
+
+const photoWrapper = (navigation: string, apps: string): string => {
   return `<div class="photo">
             ${navigation}
             <div class="photo-inner">
-              <img src="${webpack}"/>
+              ${apps}
               photo
             </div>
           </div>`;
@@ -15,7 +26,7 @@ const renderPhotoPage = (navigation: string): void => {
   const appId = $('#app') as HTMLDivElement;
 
   appId.innerHTML = '';
-  appId.insertAdjacentHTML('beforeend', photoWrapper(navigation));
+  appId.insertAdjacentHTML('beforeend', photoWrapper(navigation, photoScrollWrapper()));
 };
 
 export default renderPhotoPage;
