d549da9590b715a9dc4890d794d240be8a09965f
hochan222 <hochan049@gmail.com>
2021-08-23 19:39:09 +0900

    refactor: model code

--- src/model/memo-page.ts
+++ src/model/memo-page.ts
@@ -1,10 +1,6 @@
 export default {
   setLocalStorageMemoData(key: string, appData: string): void {
     const alarmData = this.getLocalStorageMemoData(key) ? (this.getLocalStorageMemoData(key) as []) : [];
-    // if (alarmData.length > ALARM_LIST_MAX_COUNT - 1) {
-    //   alert(STORAGE_MAXIMUN_ALARM_LIST_ERROR);
-    //   return;
-    // }
     localStorage.setItem(key, JSON.stringify([...alarmData, appData]));
   },
   getLocalStorageMemoData(key: string): string[] | null {
